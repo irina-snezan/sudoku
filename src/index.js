e91f3772dfe6dcd50cfbdd7144245aa1669a9581
Irina Shabuldajeva <70856550+irina-snezan@users.noreply.github.com>
2021-08-17 01:02:12 +0300

    feat: fix code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = function solveSudoku(matrix) {
+module.exports = solveSudoku = matrix => {
   for (let row = 0; row < 9; row++) {
     for (let col = 0; col < 9; col++) {
       if (matrix[row][col] === 0) {
